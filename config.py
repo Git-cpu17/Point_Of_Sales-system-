b1c228b6ca1873f66935d6b81a2f1d5ed6d4f0e8
Hoang Nguyen <159190184+Git-cpu17@users.noreply.github.com>
2025-10-15 19:20:33 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 import os
 
 class Config:
-    # Retrieve the MySQL connection string from environment variables
-    SQLALCHEMY_DATABASE_URI = os.getenv('AZURE_MYSQL_URI')
-
-    # Recommended: fallback local connection (optional)
-    if not SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI = (
-            'mysql+pymysql://root:localpassword@localhost/store_db'
-        )
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('AZURE_SQL_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')

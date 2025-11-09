4ad2f3ae739abc952ac55b98921a531cb7f5851a
Ben Bizman <ben.bizman@gmail.com>
2025-11-08 20:06:35 -0600

    Create department.py

---
+++ routes/department.py
@@ -0,0 +1,38 @@
+@app.route('/department')
+@with_db
+def department(cursor, conn):
+    cursor.execute("SELECT * FROM Department")
+    departments = rows_to_dict_list(cursor)
+    return render_template('department_dashboard.html', departments=departments)
+
+@app.route('/transactions')
+@with_db
+def get_transactions(cursor, conn):
+    base_query = """
+        SELECT t.TransactionID, t.TransactionDate, t.TotalAmount, t.EmployeeID, t.CustomerID, t.PaymentMethod,
+               e.Name AS EmployeeName, c.Name AS CustomerName
+        FROM SalesTransaction t
+        JOIN Employee e ON t.EmployeeID = e.EmployeeID
+        JOIN Customer c ON t.CustomerID = c.CustomerID
+    """
+    filters = []
+    params = []
+
+    if request.args.get('employee'):
+        filters.append("e.Name LIKE ?")
+        params.append(f"%{request.args['employee']}%")
+    if request.args.get('payment_method'):
+        filters.append("t.PaymentMethod = ?")
+        params.append(request.args['payment_method'])
+
+    if filters:
+        base_query += " WHERE " + " AND ".join(filters)
+
+    if request.args.get('sort_by') == 'date':
+        base_query += " ORDER BY t.TransactionDate DESC"
+    elif request.args.get('sort_by') == 'amount':
+        base_query += " ORDER BY t.TotalAmount DESC"
+
+    cursor.execute(base_query, params)
+    transactions = rows_to_dict_list(cursor)
+    return jsonify(transactions)

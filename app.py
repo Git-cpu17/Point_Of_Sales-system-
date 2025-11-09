aa5b36d51353490bbbda2cb5944797a905f01d8c
Ben Bizman <ben.bizman@gmail.com>
2025-11-08 21:12:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, make_response
+from flask_cors import CORS
+from db import with_db, rows_to_dict_list, get_db_connection
+import os
+import traceback, sys
+
+app = Flask(__name__)
+CORS(app)
+app.secret_key = os.environ.get('SECRET_KEY')
+
 # -----------------------------
 # Routes
 # -----------------------------

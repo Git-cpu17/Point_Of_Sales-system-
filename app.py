d337725d0bec6636a7c0eddf1e817baac1a17b51
Ben Bizman <ben.bizman@gmail.com>
2025-11-08 19:53:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,45 +8,6 @@ app = Flask(__name__)
 CORS(app)
 app.secret_key = os.environ.get('SECRET_KEY')
 
-# -----------------------------
-# Routes
-# -----------------------------
-
-@app.route('/')
-@with_db
-def home(cursor, conn):
-    cursor.execute("SELECT * FROM Product")
-    products = rows_to_dict_list(cursor)
-    cursor.execute("SELECT * FROM Department")
-    departments = rows_to_dict_list(cursor)
-
-    user = None
-    role = session.get('role')
-
-    if role == 'customer':
-        cursor.execute("SELECT Name FROM Customer WHERE CustomerID = ?", (session['user_id'],))
-        record = cursor.fetchone()
-        if record:
-            user = {'Name': record[0], 'role': 'customer'}
-
-    elif role == 'admin':
-        cursor.execute("SELECT Name FROM Administrator WHERE AdminID = ?", (session['user_id'],))
-        record = cursor.fetchone()
-        if record:
-            user = {'Name': record[0], 'role': 'admin'}
-
-    elif role == 'employee':
-        cursor.execute("SELECT Name FROM Employee WHERE EmployeeID = ?", (session['user_id'],))
-        record = cursor.fetchone()
-        if record:
-            user = {'Name': record[0], 'role': 'employee'}
-
-    return render_template('index.html', products=products, departments=departments, user=user)
-
-@app.route("/api/status", methods=["GET"])
-def status():
-    return jsonify({"message": "Flask API is running and connected to Azure SQL!"})
-
 @app.route("/products", methods=["GET"])
 @with_db
 def get_products(cursor, conn):

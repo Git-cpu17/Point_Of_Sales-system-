8f457077064f9ef6903c4694e75c7836a56b645c
Ben Bizman <ben.bizman@gmail.com>
2025-11-08 20:14:05 -0600

    Update employee.py

--- routes/employee.py
+++ routes/employee.py
@@ -1,3 +1,9 @@
+from flask import Blueprint, render_template, request, jsonify, session, redirect, url_for
+from db import with_db, rows_to_dict_list
+from utility.security import require_role
+
+bp = Blueprint("employee", __name__)
+
 @app.route('/employee')
 @with_db
 def employee_dashboard(cursor, conn):
